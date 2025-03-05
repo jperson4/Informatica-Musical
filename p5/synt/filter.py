885f1f64c2127b74b7a124f1856d9f4a45fa48e2
antipersona <dla.cruz2002@gmail.com>
2025-03-05 15:26:16 +0100

    filtro

---
+++ p5/synt/filter.py
@@ -0,0 +1,50 @@
+
+from synt.function import *
+from synt.const import *
+from tkinter import *
+
+from synt.osc import *
+
+# TODO ver si satura
+class FilterIIR(Function):
+    ''' recibe una funcion (generador de ondas) y devuelve su onda filtrada'''
+    def __init__(self,signal:Function,alpha:Function):
+        self.signal = signal
+        self.mem = 0
+        self.alpha = alpha
+        # self.step = step 
+        # por defecto inactivo
+        self.act = False
+
+    def fun(self, tiempo):
+        data = self.signal.next(tiempo)
+        if self.act:
+            data[0] = self.mem + self.alpha.next(tiempo) * (data[0]-self.mem)
+            for i in range(1,CHUNK):
+                data[i] = data[i-1] + self.alpha.next(tiempo) * (data[i]-data[i-1])
+            self.mem = data[CHUNK-1]
+        self.mem = data[-1] # actualizamos memo con ultima muestra
+        return data
+
+    def activate(self):
+        self.act = True
+
+    def deactivate(self):
+        self.act = False    
+
+    def isActive(self):
+        return self.act
+
+    def doShow(self, tk, bg="#808090", side=TOP):
+        _tk = super().doShow(tk, bg, side=TOP)
+        # TODO hacer un boton para activar/desactivar
+        self.alpha.addNombre('alpha')
+        self.alpha.doShow(_tk, bg, side)
+        
+        return _tk
+    # no hace falta teniendo el do show
+    # def upAlpha(self):
+        # self.alpha = min(2.0,max(0.1,self.alpha+self.step))
+
+    # def downAlpha(self):
+        # self.alpha = min(2.0,max(0.1,self.alpha-self.step))
