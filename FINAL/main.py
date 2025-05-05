2164b5580cb25d39fd09038c001dfd3003b78ce4
antipersona <dla.cruz2002@gmail.com>
2025-05-05 20:41:55 +0200

    nos mudamos a nativo xq mucha vaina

--- FINAL/main.py
+++ FINAL/main.py
@@ -1,20 +1,5 @@
 from audio import *
-import sounddevice as sd
 from pyo import *
 
 if __name__ == "__main__":
-    pa_list_devices()
-    server = Server()
-    server.amp = AMP
-    server.setSamplingRate(SRATE)
-    server.setBufferSize(CHUNK)
-    server.setInputDevice(DEVICE_IN)
-    server.setOutputDevice(DEVICE_OUT)
-    server.boot()
-
-    # seno
-    a = Sine(440).out()
-    # b = Sine(540).out()
-    
-    server.gui(locals())
-    # server.start()
+    server = start_server()
