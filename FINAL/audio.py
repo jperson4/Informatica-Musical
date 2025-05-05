a98fb461d6fa390f83eed6c8da4ff6698062816a
antipersona <dla.cruz2002@gmail.com>
2025-05-05 19:18:19 +0200

    reorganizar todo un poco

--- FINAL/audio.py
+++ FINAL/audio.py
@@ -4,8 +4,8 @@ from pyo import *
 CHUNK = 1024  # Número de muestras por buffer
 SRATE = 44100  # Frecuencia de muestreo
 AMP = 0.1 # Amplitud general del sonido
-DEVICE_OUT = 5  # Dispositivo de audio a utilizar (puede cambiar según el sistema)
-DEVICE_IN = 5  # Dispositivo de audio a utilizar (puede cambiar según el sistema)
+DEVICE_OUT = 6  # Dispositivo de audio a utilizar (puede cambiar según el sistema)
+DEVICE_IN = 6  # Dispositivo de audio a utilizar (puede cambiar según el sistema)
 
 # input = None  # Variable para almacenar el objeto de entrada de audio
 
@@ -25,12 +25,12 @@ DEVICE_IN = 5  # Dispositivo de audio a utilizar (puede cambiar según el sistem
 
 def start_server():
     """Inicia el servidor de audio y lo configura."""
-    server = Server(nchnls=2)
+    server = Server(nchnls=1)
     server.amp = AMP
     server.setSamplingRate(SRATE)
     server.setBufferSize(CHUNK)
-    server.setInputDevice(DEVICE_IN)
-    server.setOutputDevice(DEVICE_OUT)
-    server.setNchnls(2)  # Número de canales de salida (2 para estéreo)
+    server.setInputDevice(DEVICE_IN) # & = default
+    server.setOutputDevice(DEVICE_OUT) # & = default
+    server.setNchnls(1)  # Número de canales de salida aparentemente solo vale 1
     server.boot()
     return server
