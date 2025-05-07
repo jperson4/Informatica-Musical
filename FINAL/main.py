971914555ff708fdef04ae60b16e863dc01dd1f5
antipersona <dla.cruz2002@gmail.com>
2025-05-07 14:00:14 +0200

    haciendo prenota

--- FINAL/main.py
+++ FINAL/main.py
@@ -1,5 +1,14 @@
 from audio import *
 from pyo import *
+from synt.nota import Nota
 
 if __name__ == "__main__":
     server = start_server()
+    server.start()
+    nota = Nota(440, Sine(440))
+    nota.out()  
+    # nota.play()
+    # time.sleep(1)
+    # nota.stop()
+    # time.sleep(1)
+    server.gui(locals())
